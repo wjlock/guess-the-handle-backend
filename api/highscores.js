9a1a8d65a7d62d77e2223cb59efee117902bf5f3
Will <will.locklier@gmail.com>
2021-01-24 20:09:32 -0500

    Add twitter api call

--- api/highscores.js
+++ api/highscores.js
@@ -6,13 +6,35 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
 const JWT_SECRET = process.env.JWT_SECRET;
+var Twitter = require('twitter');
+var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    bearer_token: process.env.TWITTER_BEARER_TOKEN
+  });
 
 // Models
 const db = require('../models');
 
-// GET api/books/test (Public)
+// GET api/highscores/test (Public)
 router.get('/test', (req, res) => {
     res.json({ msg: 'Highscores endpoint OK!'});
 });
+
+useEffect(() => {
+    client.get('search/tweets.json?q=sports&result_type=popular', function(error, response) {
+        if(error) throw error;
+        console.log(response);  // Raw response object.
+      });
+
+})
+
+//Test route for twitter API
+router.get('/twitter', (req, res) => {
+    client.get(''search/tweets.json?q=sports&result_type=popular', function(error, response) {
+        if(error) throw error;
+        console.log(response);')
+        
+
 
 module.exports = router;
